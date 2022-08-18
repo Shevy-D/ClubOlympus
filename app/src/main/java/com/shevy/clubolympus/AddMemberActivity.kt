aa684da0711bd16df29156bca25f1b7624259278
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-18 18:13:32 +0500

    Added ClubOlympusContract

--- app/src/main/java/com/shevy/clubolympus/AddMemberActivity.kt
+++ app/src/main/java/com/shevy/clubolympus/AddMemberActivity.kt
@@ -1,11 +1,18 @@
 package com.shevy.clubolympus
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
+import com.shevy.clubolympus.data.ClubOlympusContract.MemberEntry
 import com.shevy.clubolympus.databinding.ActivityAddMemberBinding
-import com.shevy.clubolympus.databinding.ActivityMainBinding
 
 class AddMemberActivity : AppCompatActivity() {
     lateinit var binding: ActivityAddMemberBinding
@@ -14,6 +21,8 @@ class AddMemberActivity : AppCompatActivity() {
     private lateinit var lastNameEditText: EditText
     private lateinit var groupNameEditText: EditText
     private lateinit var genderSpinner: Spinner
+
+    //private lateinit var spinnerAdapter: ArrayAdapter<String>
     private var gender: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +34,46 @@ class AddMemberActivity : AppCompatActivity() {
         lastNameEditText = binding.etLastName
         groupNameEditText = binding.etGroup
         genderSpinner = binding.spGender
+
+        val spinnerAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.array_gender,
+            android.R.layout.simple_spinner_item
+        )
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        genderSpinner.adapter = spinnerAdapter
+        genderSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val selectedGender: String = p0?.getItemAtPosition(p2) as String
+                if (!TextUtils.isEmpty(selectedGender)) {
+                    gender = when (selectedGender) {
+                        "Male" -> MemberEntry.GENDER_MALE
+                        "Female" -> MemberEntry.GENDER_FEMALE
+                        else -> MemberEntry.GENDER_UNKNOWN
+                    }
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                gender = 0
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.edit_member_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_member -> return true
+            R.id.delete_member -> return true
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/shevy/clubolympus/data/ClubOlympusContract.kt
@@ -0,0 +1,34 @@
+package com.shevy.clubolympus.data
+
+import android.content.ContentResolver
+import android.net.Uri
+import android.provider.BaseColumns
+
+object ClubOlympusContract {
+
+/*    const val DATABASE_VERSION = 1
+    const val DATABASE_NAME = "olympus"
+    const val SCHEME = "content://"
+    const val AUTHORITY = "com.example.clubolympus"
+    const val PATH_MEMBERS = "members"
+    val BASE_CONTENT_URI: Uri = Uri.parse(SCHEME + AUTHORITY)*/
+
+    object MemberEntry : BaseColumns {
+        const val TABLE_NAME = "members"
+        const val _ID = BaseColumns._ID
+        const val COLUMN_FIRST_NAME = "firstName"
+        const val COLUMN_LAST_NAME = "lastName"
+        const val COLUMN_GENDER = "gender"
+        const val COLUMN_SPORT = "sport"
+
+        const val GENDER_UNKNOWN = 0
+        const val GENDER_MALE = 1
+        const val GENDER_FEMALE = 2
+
+/*        val CONTENT_URI: Uri = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_MEMBERS)
+        const val CONTENT_MULTIPLE_ITEMS = ContentResolver.CURSOR_DIR_BASE_TYPE +
+                "/" + AUTHORITY + "/" + PATH_MEMBERS
+        const val CONTENT_SINGLE_ITEM = ContentResolver.CURSOR_ITEM_BASE_TYPE +
+                "/" + AUTHORITY + "/" + PATH_MEMBERS*/
+    }
+}
