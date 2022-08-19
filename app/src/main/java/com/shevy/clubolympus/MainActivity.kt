400ef2b4638650c7560532981aff594777bd8e26
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-19 15:52:09 +0500

    Implemented CursorLoader

--- app/src/main/java/com/shevy/clubolympus/MainActivity.kt
+++ app/src/main/java/com/shevy/clubolympus/MainActivity.kt
@@ -6,14 +6,17 @@ import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import com.shevy.clubolympus.data.ClubOlympusContract.MemberEntry
 import com.shevy.clubolympus.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity(),
-LoaderManager.LoaderCallbacks<Cursor>{
+    LoaderManager.LoaderCallbacks<Cursor> {
     lateinit var binding: ActivityMainBinding
+    private val MEMBER_LOADER = 123
+    lateinit var memberCursorAdapter: MemberCursorAdapter
 
     lateinit var dataListView: ListView
 
@@ -28,37 +31,29 @@ LoaderManager.LoaderCallbacks<Cursor>{
         floatingActionButton.setOnClickListener {
             startActivity(Intent(this, AddMemberActivity::class.java))
         }
+
+        memberCursorAdapter = MemberCursorAdapter(this, null, false)
+        dataListView.adapter = memberCursorAdapter
+
+        supportLoaderManager.initLoader(MEMBER_LOADER, null, this)
     }
 
-    override fun onStart() {
-        super.onStart()
-        displayData()
-    }
-
-    private fun displayData() {
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
         val projection = arrayOf(
             MemberEntry._ID,
             MemberEntry.COLUMN_FIRST_NAME,
             MemberEntry.COLUMN_LAST_NAME,
-            MemberEntry.COLUMN_GENDER,
             MemberEntry.COLUMN_SPORT
         )
 
-        val cursor = contentResolver.query(MemberEntry.CONTENT_URI, projection, null, null, null)
-        val cursorAdapter = MemberCursorAdapter(this, cursor, false)
-        dataListView.adapter = cursorAdapter
-
+        return CursorLoader(this, MemberEntry.CONTENT_URI, projection, null, null, null)
     }
 
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        TODO("Not yet implemented")
-    }
-
-    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        TODO("Not yet implemented")
+    override fun onLoadFinished(loader: Loader<Cursor>, cursor: Cursor?) {
+        memberCursorAdapter.swapCursor(cursor)
     }
 
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        TODO("Not yet implemented")
+        memberCursorAdapter.swapCursor(null)
     }
 }
