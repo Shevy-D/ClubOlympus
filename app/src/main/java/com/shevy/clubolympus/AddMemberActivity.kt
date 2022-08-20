ce5a860ac8c90d7307ccd3ca647d5f85394e4017
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-20 11:51:27 +0500

    Loading single record data

--- app/src/main/java/com/shevy/clubolympus/AddMemberActivity.kt
+++ app/src/main/java/com/shevy/clubolympus/AddMemberActivity.kt
@@ -1,6 +1,8 @@
 package com.shevy.clubolympus
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.Menu
@@ -9,16 +11,25 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NavUtils
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import com.shevy.clubolympus.data.ClubOlympusContract.MemberEntry
 import com.shevy.clubolympus.databinding.ActivityAddMemberBinding
 
-class AddMemberActivity : AppCompatActivity() {
+
+class AddMemberActivity : AppCompatActivity(),
+    LoaderManager.LoaderCallbacks<Cursor>{
     lateinit var binding: ActivityAddMemberBinding
+
+    private val EDIT_MEMBER_LOADER = 123
 
     private lateinit var firstNameEditText: EditText
     private lateinit var lastNameEditText: EditText
     private lateinit var sportEditText: EditText
     private lateinit var genderSpinner: Spinner
+    private lateinit var currentMemberUri: Uri
+    private var currUri: Uri? = null
 
     private var gender: Int = 0
 
@@ -27,8 +38,15 @@ class AddMemberActivity : AppCompatActivity() {
         binding = ActivityAddMemberBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = intent
-        val currentMemberUri = intent.data
+        currUri = intent.data
+        if (currUri == null) {
+            invalidateOptionsMenu()
+            title = "Add a member"
+        } else {
+            currentMemberUri = currUri ?: MemberEntry.CONTENT_URI
+            title = "Edit the member"
+            LoaderManager.getInstance(this).initLoader(EDIT_MEMBER_LOADER, null, this)
+        }
 
         title = if (currentMemberUri == null) {
             "Add a Member"
@@ -64,6 +82,8 @@ class AddMemberActivity : AppCompatActivity() {
                 gender = 0
             }
         }
+
+        supportLoaderManager.initLoader(EDIT_MEMBER_LOADER, null, this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -105,5 +125,41 @@ class AddMemberActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        val projection = arrayOf(
+            MemberEntry._ID,
+            MemberEntry.COLUMN_FIRST_NAME,
+            MemberEntry.COLUMN_LAST_NAME,
+            MemberEntry.COLUMN_GENDER,
+            MemberEntry.COLUMN_SPORT
+        )
+        return CursorLoader(this, currentMemberUri, projection, null, null, null)
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor?>, cursor: Cursor) {
+        if (cursor.moveToFirst()) {
+            val firstNameColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_FIRST_NAME)
+            val lastNameColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_LAST_NAME)
+            val genderColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_GENDER)
+            val sportColumnIndex = cursor.getColumnIndex(MemberEntry.COLUMN_SPORT)
+            val firstName = cursor.getString(firstNameColumnIndex)
+            val lastName = cursor.getString(lastNameColumnIndex)
+            val gender = cursor.getInt(genderColumnIndex)
+            val sport = cursor.getString(sportColumnIndex)
+            firstNameEditText.setText(firstName)
+            lastNameEditText.setText(lastName)
+            sportEditText.setText(sport)
+            when (gender) {
+                MemberEntry.GENDER_MALE -> genderSpinner.setSelection(1)
+                MemberEntry.GENDER_FEMALE -> genderSpinner.setSelection(2)
+                MemberEntry.GENDER_UNKNOWN -> genderSpinner.setSelection(0)
+            }
+        }
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
